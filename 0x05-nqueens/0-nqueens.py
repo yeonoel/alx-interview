26610e4fcc4b99d823653a123bd5d2c0c0589f91
yeonoel <yeopevroguinoel@gmail.com>
2023-01-30 16:53:09 +0000

    NXNqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+
+def solve_nqueens(n, i, a, b, c, solution=[]):
+    if i == n:
+        yield solution
+        return
+    for j in range(n):
+        if j not in a and i+j not in b and i-j not in c:
+            yield from solve_nqueens(n, i+1, a+[j], b+[i+j], c+[i-j], solution + [[i, j]])
+
+def nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    for solution in solve_nqueens(n, 0, [], [], []):
+        print (solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    nqueens(n)
+
