6258bbddca1b668d579eddf87ee3da757f787a4b
yeonoel <yeopevroguinoel@gmail.com>
2023-02-23 22:19:48 +0000

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+"""Change comes from within"""
+
+def makeChange(coins, total):
+    """Returns fewest number of coins needed to meet total."""
+    n = len(coins)
+    dp = [-1] * (total + 1)
+    dp[0] = 0
+    coins = sorted(coins)
+    for i in range(1, n + 1):
+        for j in range(coins[i - 1], total + 1):
+            if j == coins[i - 1]:
+                dp[j] = 1
+            elif dp[j - coins[i - 1]] != -1:
+                if dp[j] == -1:
+                    dp[j] = dp[j - coins[i - 1]] + 1
+                else:
+                    dp[j] = min(dp[j], dp[j - coins[i - 1]] + 1)
+    return dp[total]
