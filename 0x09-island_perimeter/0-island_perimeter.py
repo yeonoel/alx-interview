14ba3d8e7ef59a829f4185c1136e85091cb00542
yeonoel <yeopevroguinoel@gmail.com>
2023-03-01 22:58:03 +0000

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,19 +1,22 @@
-def island_perimeter(grid):
-""" initialize perimeter to """
-    perimeter = 0
-    
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:  # if the cell contains land
-                # check if the cell is on the perimeter
-                if i == 0 or grid[i-1][j] == 0:  # top edge
-                    perimeter += 1
-                if j == 0 or grid[i][j-1] == 0:  # left edge
-                    perimeter += 1
-                if i == len(grid)-1 or grid[i+1][j] == 0:  # bottom edge
-                    perimeter += 1
-                if j == len(grid[0])-1 or grid[i][j+1] == 0:  # right edge
-                    perimeter += 1
-    
-    return perimeter
+#!/usr/bin/python3
+""" Island perimeter"""
 
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid. """
+    perimeter = 0
+    grid_len = len(grid)
+    col = len(grid[0]) if grid_len else 0
+
+    for i in range(grid_len):
+        row_len = len(grid[i])
+        for j in range(row_len):
+            index = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
+            check = [1 if k[0] in range(grid_len) and k[1] in range(col)
+                     else 0 for k in index]
+
+            if grid[i][j]:
+                perimeter += sum([1 if not r or not grid[k[0]][k[1]] else 0
+                                  for r, k in zip(check, index)])
+
+    return perimeter
